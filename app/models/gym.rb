928f28c7af2ac6a9123fde86c17ac2ff29e0b72d
Steven McGrath <mcgrath.steven7@gmail.com>
2016-07-12 16:11:20 -0400

    fixing seed file

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,5 +1,5 @@
 class Gym < ActiveRecord::Base
   has_many :users
   has_many :trainers, through: :users
-  has_one :leader
+  has_one :leader, through: :users
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,37 @@
-pewter_city = Gym.create(name: 'Pewter City Gym')
-leader = Leader.create
-pokedex = Pokedex.create
-bulbasaur = Pokemon.create(name: 'Bulbasaur', element: 'grass', rarity: 1, evolution_state: 1, number_of_evolutions: 2, starting_level: 4)
-my_bulbasaur = TrainerSpecificPokemon.create(level: bulbasaur.starting_level)
-trainer_1 = Trainer.create
-trainer_2 = Trainer.create
-trainer_3 = Trainer.create
-lee = User.create(name: 'Lee', age: 21, catchphrase: 'ayy lmao', rank: 9001)
-gina = User.create(name: 'Gina', age: 21, catchphrase: 'idk lol??', rank: 19)
-steven = User.create(name: 'Steven', age: 26, catchphrase: 'here comes dat boi', rank: 1)
-julie = User.create(name: 'Julie', age: 25, catchphrase: 'whatever lmao amirite', rank: 2)
+Gym.destroy_all
+User.destroy_all
+Trainer.destroy_all
+Leader.destroy_all
+Pokemon.destroy_all
+Pokedex.destroy_all
+TrainerSpecificPokemon.destroy_all 
+def gym
+Gym.create(name: 'Pewter City Gym')
+end
+def users
+  User.create(name: 'Lee', age: 21, catchphrase: 'ayy lmao', rank: 9001, gym_id: 1)
+  User.create(name: 'Gina', age: 21, catchphrase: 'idk lol??', rank: 19, gym_id: 1)
+  User.create(name: 'Steven', age: 26, catchphrase: 'here comes dat boi', rank: 1, gym_id: 1)
+  User.create(name: 'Julie', age: 25, catchphrase: 'whatever lmao amirite', rank: 2, gym_id: 1)
+end
+def leader
+  leader = Leader.create(user_id: 1)
+end
+def pokedex
+  User.all.each do |user|
+    Pokedex.create(user_id: user.id)
+  end
+end
+def trainers
+ Trainer.create(user_id: 2)
+ Trainer.create(user_id: 3)
+ Trainer.create(user_id: 4)
+end
+Pokemon.create(name: 'Bulbasaur', element: 'grass', rarity: 1, evolution_state: 1, number_of_evolutions: 2, starting_level: 4)
+TrainerSpecificPokemon.create(level: Pokemon.first.starting_level)
+gym
+users
+leader
+pokedex
+trainers
+
