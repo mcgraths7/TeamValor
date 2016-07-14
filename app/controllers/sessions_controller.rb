fe7cb5efa4894e0105735583708a7dbbbbe5ac99
JulieMarin <jfm7391@gmail.com>
2016-07-14 10:13:31 -0400

    add fix to address login bug when input fields left blank

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
   def create
 
     user = User.find_by(name: session_params[:name])
-    byebug
-    if user.valid? && user.authenticate(session_params[:password])
+    if !!user && user.valid? && user.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
@@ -22,6 +21,10 @@ class SessionsController < ApplicationController
   end
 
   private
+
+  def exists_and_valid?
+
+  end
 
   def session_params
     params.require(:user).permit(:name, :password)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :password, presence: true
+  validates :name, presence: true
+
 end
