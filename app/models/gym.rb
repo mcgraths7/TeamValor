f63af2f00fdb81dd52dd00134bb1a258f0521079
JeffKatzy <JeffreyEricKatz@gmail.com>
2016-07-14 00:05:33 -0400

    not sure about find-leader method

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -2,7 +2,7 @@ class Gym < ApplicationRecord
   has_many :users
   has_many :trainers, through: :users
   has_one :leader, through: :users
-
+# leader (rename to leader not find_leader, and shouldn't leader return this from me, from the association?)
   def find_leader
     self.users.joins(:leader).first
   end
