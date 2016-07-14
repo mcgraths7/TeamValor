fd544b1618a6c5a1cc2910ac5dd5a7681fc57d1f
leearaneta <lee.araneta@gmail.com>
2016-07-14 15:50:18 -0400

    coldplay sucks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_one :trainer
   has_one :leader
   has_many :user_pokemons
-  # accepts_nested_attributes_for 
+  # accepts_nested_attributes_for
   has_secure_password
 
   # validates :password, presence: true
