4944393651cbf5bde012996879c766509f636870
JulieMarin <jfm7391@gmail.com>
2016-07-14 16:24:16 -0400

    dat boi

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,8 @@ class User < ApplicationRecord
   has_many :user_pokemons
   # accepts_nested_attributes_for
   has_secure_password
-<<<<<<< HEAD
   # validates :password, presence: true
   # validates :name, presence: true
   validates_presence_of :name, :password
->>>>>>> ba4e249e1208c836b3530ddd2d9013275ccaafe3
 
 end
