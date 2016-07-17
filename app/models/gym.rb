1ad895e66b791fa528bd59126e0c575b4519153d
leearaneta <lee.araneta@gmail.com>
2016-07-17 01:39:57 -0400

    a little more leader functionality

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -3,6 +3,18 @@ class Gym < ApplicationRecord
   has_many :trainers, through: :users
   has_one :leader, through: :users # this association doesn't work for some reason
 
+  def top_tier_users
+    self.users.joins(:trainer).where('rank > 25').order('rank DESC')
+  end
+
+  def mid_tier_users
+    self.users.joins(:trainer).where('trainers.rank BETWEEN 11 AND 24').order('rank DESC')
+  end
+
+  def low_tier_users
+    self.users.joins(:trainer).where('rank < 10').order('rank DESC')
+  end
+
   def find_leader
     self.users.joins(:leader)
   end
